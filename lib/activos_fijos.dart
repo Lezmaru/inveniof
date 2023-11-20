b40a3e3837321f8545a663013ee4de500a769007
Lezmaru <luis.perez@ucb.edu.bo>
2023-11-20 01:44:12 -0400

    Añadiendo la segunda pantalla

---
+++ lib/activos_fijos.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:invenio2/main.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Invenio',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: ActivoFijoScreen(),
+    );
+  }
+}
+
+class ActivoFijoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                );
+              },
+              child: Text('Inicio'),
+              style: TextButton.styleFrom(primary: Colors.white),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Activos Fijos'),
+              style: TextButton.styleFrom(primary: Colors.white),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Gráficos'),
+              style: TextButton.styleFrom(primary: Colors.white),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Ventas'),
+              style: TextButton.styleFrom(primary: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          const Image(image: AssetImage('assets/Invenio.png'), height: 200),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Buscar Productos',
+                  ),
+                ),
+              ),
+              SizedBox(width: 20.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Buscar'),
+              ),
+            ],
+          ),
+          DataTable(
+            columns: const <DataColumn>[
+              DataColumn(
+                label: Text(
+                  'Serie',
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Tipo',
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Modelo',
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Estado',
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Fecha de Entrega',
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Fecha de Salida',
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Asignado a',
+                ),
+              ),
+            ],
+            rows: const <DataRow>[
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('MJ04Q34RST')),
+                  DataCell(Text('AIO')),
+                  DataCell(Text('M810')),
+                  DataCell(Text('En uso')),
+                  DataCell(Text('10/11/2023')),
+                  DataCell(Text('12/11/2023')),
+                  DataCell(Text('Jose Luis Perales')),
+                ],
+              ),
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('SQ461YMN12')),
+                  DataCell(Text('Televisor')),
+                  DataCell(Text('E404')),
+                  DataCell(Text('Mal Estado')),
+                  DataCell(Text('5/11/2023')),
+                  DataCell(Text(' - ')),
+                  DataCell(Text('Almacen de Soporte')),
+                ],
+              ),
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('ZZ34T56T7')),
+                  DataCell(Text('Verifone')),
+                  DataCell(Text('VX520')),
+                  DataCell(Text('En uso')),
+                  DataCell(Text('1/01/2023')),
+                  DataCell(Text('2/11/2023')),
+                  DataCell(Text('Agencia Villa Fátima')),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:invenio2/activos_fijos.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,12 +27,22 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                );
+              },
               child: Text('Inicio'),
               style: TextButton.styleFrom(primary: Colors.white),
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ActivoFijoScreen()),
+                );
+              },
               child: Text('Activos Fijos'),
               style: TextButton.styleFrom(primary: Colors.white),
             ),
@@ -74,80 +85,6 @@ class MyHomePage extends StatelessWidget {
               ElevatedButton(
                 onPressed: () {},
                 child: Text('Buscar'),
-              ),
-            ],
-          ),
-          DataTable(
-            columns: const <DataColumn>[
-              DataColumn(
-                label: Text(
-                  'Serie',
-                ),
-              ),
-              DataColumn(
-                label: Text(
-                  'Tipo',
-                ),
-              ),
-              DataColumn(
-                label: Text(
-                  'Modelo',
-                ),
-              ),
-              DataColumn(
-                label: Text(
-                  'Estado',
-                ),
-              ),
-              DataColumn(
-                label: Text(
-                  'Fecha de Entrega',
-                ),
-              ),
-              DataColumn(
-                label: Text(
-                  'Fecha de Salida',
-                ),
-              ),
-              DataColumn(
-                label: Text(
-                  'Asignado a',
-                ),
-              ),
-            ],
-            rows: const <DataRow>[
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(Text('MJ04Q34RST')),
-                  DataCell(Text('AIO')),
-                  DataCell(Text('M810')),
-                  DataCell(Text('En uso')),
-                  DataCell(Text('10/11/2023')),
-                  DataCell(Text('12/11/2023')),
-                  DataCell(Text('Jose Luis Perales')),
-                ],
-              ),
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(Text('SQ461YMN12')),
-                  DataCell(Text('Televisor')),
-                  DataCell(Text('E404')),
-                  DataCell(Text('Mal Estado')),
-                  DataCell(Text('5/11/2023')),
-                  DataCell(Text(' - ')),
-                  DataCell(Text('Almacen de Soporte')),
-                ],
-              ),
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(Text('ZZ34T56T7')),
-                  DataCell(Text('Verifone')),
-                  DataCell(Text('VX520')),
-                  DataCell(Text('En uso')),
-                  DataCell(Text('1/01/2023')),
-                  DataCell(Text('2/11/2023')),
-                  DataCell(Text('Agencia Villa Fátima')),
-                ],
               ),
             ],
           ),
