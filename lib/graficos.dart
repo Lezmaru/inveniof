41fecb868a38f241f543dbe34399c97d1f96b339
Lezmaru <luis.perez@ucb.edu.bo>
2023-12-01 02:49:19 -0400

    Ultra Pantalla de Gráficos full hd 4k link mega

--- lib/graficos.dart
+++ lib/graficos.dart
@@ -79,43 +79,69 @@ class _GraficoState extends State<GraficoScreen> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: BarChart(
-          BarChartData(
-            barGroups: [
-              BarChartGroupData(x: 0, barRods: [
-                BarChartRodData(fromY: 3, color: Colors.grey, toY: 10),
-              ], showingTooltipIndicators: [
-                0
-              ]),
-              BarChartGroupData(x: 1, barRods: [
-                BarChartRodData(fromY: 10, color: Colors.grey, toY: 10),
-              ], showingTooltipIndicators: [
-                0
-              ]),
-            ],
-            titlesData: FlTitlesData(
-              show: true,
-              leftTitles: AxisTitles(
-                sideTitles: SideTitles(showTitles: false),
-              ),
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  getTitlesWidget: (value, meta) {
-                    switch (value.toInt()) {
-                      case 0:
-                        return Text('Claudio Miguel Burgos');
-                      case 1:
-                        return Text('Jorge Armando Moscoso');
-                      default:
-                        return Text('');
-                    }
-                  },
+        padding: const EdgeInsets.all(35.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Gráfico de Ventas:',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: BarChart(
+                BarChartData(
+                  maxY: 10,
+                  barGroups: [
+                    BarChartGroupData(x: 0, barRods: [
+                      BarChartRodData(fromY: 0, color: Colors.black, toY: 5),
+                    ], showingTooltipIndicators: [
+                      0
+                    ]),
+                    BarChartGroupData(x: 1, barRods: [
+                      BarChartRodData(fromY: 0, color: Colors.black, toY: 3),
+                    ], showingTooltipIndicators: [
+                      0
+                    ]),
+                  ],
+                  titlesData: FlTitlesData(
+                    show: true,
+                    leftTitles: AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: (value, meta) {
+                          switch (value.toInt()) {
+                            case 0:
+                              return Text('Claudio Miguel Burgos');
+                            case 1:
+                              return Text('Jorge Armando Moscoso');
+                            default:
+                              return Text('');
+                          }
+                        },
+                      ),
+                    ),
+                    topTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: (value, meta) {
+                          switch (value.toInt()) {
+                            case 0:
+                              return Text('Laptops');
+                            case 1:
+                              return Text('Televisores');
+                            default:
+                              return Text('');
+                          }
+                        },
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
