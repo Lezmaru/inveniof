82739512d5be13b0298e9cec5e3ebf6a6776e4c5
Lezmaru <luis.perez@ucb.edu.bo>
2023-12-01 02:28:20 -0400

    Gráficos beta pro max

--- lib/graficos.dart
+++ lib/graficos.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:invenio2/activos_fijos.dart';
 import 'package:invenio2/main.dart';
 import 'package:invenio2/ventas.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(MyApp());
@@ -75,6 +76,46 @@ class _GraficoState extends State<GraficoScreen> {
               style: TextButton.styleFrom(primary: Colors.black),
             ),
           ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: BarChart(
+          BarChartData(
+            barGroups: [
+              BarChartGroupData(x: 0, barRods: [
+                BarChartRodData(fromY: 3, color: Colors.grey, toY: 10),
+              ], showingTooltipIndicators: [
+                0
+              ]),
+              BarChartGroupData(x: 1, barRods: [
+                BarChartRodData(fromY: 10, color: Colors.grey, toY: 10),
+              ], showingTooltipIndicators: [
+                0
+              ]),
+            ],
+            titlesData: FlTitlesData(
+              show: true,
+              leftTitles: AxisTitles(
+                sideTitles: SideTitles(showTitles: false),
+              ),
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  getTitlesWidget: (value, meta) {
+                    switch (value.toInt()) {
+                      case 0:
+                        return Text('Claudio Miguel Burgos');
+                      case 1:
+                        return Text('Jorge Armando Moscoso');
+                      default:
+                        return Text('');
+                    }
+                  },
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
