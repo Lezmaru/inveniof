60fdf00287b4ebf9515708026834522774bca11a
Crisale7 <cristhian.yanez@ucb.edu.bo>
2023-12-09 00:13:16 -0400

    Auth 0 mobil implementado

--- lib/login.dart
+++ lib/login.dart
@@ -3,6 +3,7 @@ import 'package:auth0_flutter/auth0_flutter_web.dart';
 import 'package:flutter/material.dart';
 import 'package:invenio2/main.dart';
 
+
 class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
 
@@ -12,16 +13,14 @@ class MainView extends StatefulWidget {
 
 class _MainViewState extends State<MainView> {
   Credentials? _credentials;
+   late Auth0 auth0;
 
-  late Auth0Web auth0 = Auth0Web(
-      's9I74zQAyfSkLzVtKgeiBZPGgcjWRJz6', 'dev-2xtlx3etsrdkrcyq.us.auth0.com');
 
   @override
   void initState() {
     super.initState();
-    auth0.onLoad().then((final credentials) => setState(() {
-          _credentials = credentials;
-        }));
+    auth0 = Auth0('dev-et7ad23cy60xt03a.us.auth0.com', '4hIn3NfQdPcMckaPeqD7iY49QLNOeIKm');
+   
   }
 
   @override
@@ -36,17 +35,21 @@ class _MainViewState extends State<MainView> {
           children: <Widget>[
             if (_credentials == null)
               ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
+                    
+                    final credentials =
+                        await auth0.webAuthentication().login();
+
+                    setState(() {
+                      _credentials = credentials;
+                    });
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => MyHomePage()),
                     );
                   },
                   child: const Text("Log in"))
-            else
-              ElevatedButton(
-                  onPressed: () => auth0.logout(),
-                  child: const Text("Log out")),
+  
           ],
         ),
       ),
